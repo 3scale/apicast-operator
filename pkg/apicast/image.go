3e4ce2d0cd809871989f991f49cfa5cf1044feec
Eguzki Astiz Lezaun <eastizle@redhat.com>
2022-01-17 13:48:32 +0100

    prepare 2.12-candidate: Update apicast default image URL

--- pkg/apicast/image.go
+++ pkg/apicast/image.go
@@ -2,7 +2,7 @@ package apicast
 
 import "github.com/3scale/apicast-operator/pkg/helper"
 
-const defaultImageVersion = "quay.io/3scale/apicast:latest"
+const defaultImageVersion = "quay.io/3scale/3scale212:apicast-3scale-2.12.0-GA"
 
 func GetDefaultImageVersion() string {
 	return helper.GetEnvVar("RELATED_IMAGE_APICAST", defaultImageVersion)
