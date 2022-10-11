33f80913f80b1ec4c8d7c368949b314e262c6d0e
Eguzki Astiz Lezaun <eastizle@redhat.com>
2022-10-11 15:39:04 +0200

    upgrade tooling and dependencies

--- utils.mk
+++ utils.mk
@@ -1,12 +1,12 @@
-# go-get-tool will 'go get' any package $2 and install it to $1.
-define go-get-tool
+# go-bin-install will 'go install' any package $2 and install it to $1.
+define go-bin-install
 @[ -f $(1) ] || { \
 set -e ;\
 TMP_DIR=$$(mktemp -d) ;\
 cd $$TMP_DIR ;\
 go mod init tmp ;\
 echo "Downloading $(2)" ;\
-GOBIN=$(PROJECT_PATH)/bin go get $(2) ;\
+GOBIN=$(PROJECT_PATH)/bin go install $(2) ;\
 rm -rf $$TMP_DIR ;\
 }
 endef
