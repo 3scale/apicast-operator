b9a6ae42e4b204b3199b3ddec7890f6d1ab9af95
Valery Mogilevsky <vmogilev@redhat.com>
2024-09-30 16:48:02 +0300

    THREESCALE-11385 TEST Community release of APIcast Operator v0.8.0 (2.14.1)

--- pkg/apicast/image.go
+++ pkg/apicast/image.go
@@ -2,7 +2,7 @@ package apicast
 
 import "github.com/3scale/apicast-operator/pkg/helper"
 
-const defaultImageVersion = "quay.io/3scale/apicast:latest"
+const defaultImageVersion = "quay.io/3scale/apicast:3scale-2.14.1-GA"
 
 func GetDefaultImageVersion() string {
 	return helper.GetEnvVar("RELATED_IMAGE_APICAST", defaultImageVersion)
